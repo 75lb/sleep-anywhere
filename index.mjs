7f45e4b570d1af4ae6629cdfe84432ace2b31042
Lloyd Brookes <lloyd.brookes@gmail.com>
2019-03-26 20:59:37 +0000

    docs

--- index.mjs
+++ index.mjs
@@ -1,3 +1,24 @@
-export default function sleep (ms, returnValue) {
+/**
+ * A sleep function you can use anywhere.
+ *
+ * @module sleep-anywhere
+ * @example
+ * const sleep = require('sleep-anywhere')
+ *
+ * const result = await sleep(5000, 'later')
+ * console.log('5s', result)
+ * // 5s later
+ */
+
+/**
+ * Returns a promise which fulfils after `ms` milliseconds with the supplied `returnValue`.
+ * @param {number} ms - How long in milliseconds to sleep for.
+ * @param {*} [returnValue] - The value to return.
+ * @returns {Promise}
+ * @alias module:sleep-anywhere
+ */
+function sleep (ms, returnValue) {
   return new Promise(resolve => setTimeout(() => resolve(returnValue), ms))
 }
+
+export default sleep
